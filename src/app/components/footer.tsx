f96cc877682d834bc5f00b2caad1f3bc6c5a393b
GoodbyeAntwerpblue <daishi3741@gmail.com>
2023-12-30 19:34:23 +0900

    fix

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -2,17 +2,11 @@ import { Box, Container, Flex, Heading } from "@chakra-ui/react";
 
 export function Footer() {
   return (
-    <Box
-      bgColor="gray.100"
-      p={{ base: 1, md: 1 }}
-      position="fixed"
-      width="100%"
-      bottom="0%"
-    >
-      <Container>
+    <Box bgColor="gray.100" p={{ base: 1, md: 1 }}>
+      <Container maxW={"container.lg"}>
         <Flex
           as="header"
-          py="3"
+          py="4"
           justifyContent="space-between"
           alignItems="left"
         >
